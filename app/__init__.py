4e13c674da5a8f50dcb2f3dfa97dafd3e37b7898
Gareth Funk <gareth.funk94@gmail.com>
2018-03-17 09:41:08 +0000

    flask app builder template stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,36 @@
+import logging
 from flask import Flask
+from flask.ext.appbuilder import SQLA, AppBuilder
+
+"""
+ Logging configuration
+"""
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLA(app)
+appbuilder = AppBuilder(app, db.session)
 
-@app.route('/')
-def homepage():
-	return "Hello, World!"
+
+"""
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+#Only include this for SQLLite constraints
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    # Will force sqllite contraint foreign keys
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+"""
+
+from app import views
+
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(port=8080, debug=True)
