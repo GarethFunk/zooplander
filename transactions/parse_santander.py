1c1bdc95cce0f65f06b7baac8fa85ed9e0752853
emmarocheteau <ecr38@cam.ac.uk>
2018-03-17 20:29:30 +0000

    .

--- transactions/parse_santander.py
+++ transactions/parse_santander.py
@@ -11,29 +11,34 @@ obp.setApiVersion(API_VERSION)
 # Login and set authorized token
 obp.login(USERNAME, PASSWORD, CONSUMER_KEY)
 
-our_bank = OUR_BANK
+#our_bank = OUR_BANK
 
-accounts = obp.getPrivateAccounts(our_bank)
-account = accounts[0]['id']
+#accounts = obp.getPrivateAccounts(our_bank)
+#account = accounts[0]['id']
 
-transactions = obp.get_transactions()
-transactions = transactions['json']['hits']['hits']
 
-transactions_dataframe = pd.DataFrame(columns=['Date', 'Amount', 'Currency', 'Description'])
+def get_transactions():
 
-for transaction in transactions:
-    transaction = transaction['_source']
-    if transaction['debit_credit'] == 'Credit':
-        transaction['amount'] = 0 - float(transaction['amount'])
-    transactions_temp = pd.DataFrame({
-        'Date': transaction['posted_date'],
-        'Amount': transaction['amount'],
-#        'Debit/Credit': transaction['debit_credit'],
-        'Currency': transaction['currency'],
-        'Description': transaction['description']}, index=[0])
+    transactions = obp.get_transactions()
+    transactions = transactions['json']['hits']['hits']
 
-    transactions_dataframe = pd.concat([transactions_dataframe, transactions_temp], axis=0)
+    transactions_dataframe = pd.DataFrame(columns=['Date', 'Amount', 'Currency', 'Description'])
 
-print(transactions_dataframe)
+    for transaction in transactions:
+        transaction = transaction['_source']
+        if transaction['debit_credit'] == 'Credit':
+            transaction['amount'] = 0 - float(transaction['amount'])
+        transactions_temp = pd.DataFrame({
+            'Date': transaction['posted_date'],
+            'Amount': transaction['amount'],
+            'Currency': transaction['currency'],
+            'Description': transaction['description']}, index=[0])
+        transactions_dataframe = pd.concat([transactions_dataframe, transactions_temp], axis=0)
+
+    return transactions_dataframe
+
+if __name__ == '__main__':
+    print_transactions = get_transactions()
+    print(print_transactions)
 
 
