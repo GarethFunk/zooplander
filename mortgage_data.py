83b12097a31d8b6d348307ad6fcb4c4dd06937ac
emmarocheteau <ecr38@cam.ac.uk>
2018-03-18 12:38:57 +0000

    hacky function

--- mortgage_data.py
+++ mortgage_data.py
@@ -13,7 +13,6 @@ def get_mortgages(file):
     mortgages = pd.read_csv(file, sep = '\t')
     return mortgages
 
-#house value from Abs
 def return_ranked_loans(house_value, deposit, rent, mortgages):
     #borrowed = house_value - deposit
     #print((house_value-deposit)*100/house_value)
@@ -31,17 +30,18 @@ def return_ranked_loans(house_value, deposit, rent, mortgages):
 def time_to_afford(time_projected):
     #which year will annual_deposit be more than 10%, house_price
     year = []
-    for i in len(time_projected[1]):
-        if time_projected[1]/time_projected[2] >= 0.1:
-            year.append(time_projected[0])
+    for i in range(len(time_projected[1])):
+        if time_projected[1][i]/time_projected[2][i] >= 0.1:
+            year.append(time_projected[0][i])
     return year[0]
 
 
 if __name__ == '__main__':
     mortgages = get_mortgages('mortgages_20%_200000.csv')
     ranked = return_ranked_loans(250000, 25000, 1000, mortgages)
-    #tuple_thing = pred_10y_prices('W12', 10000, 250000)
-    tuple_thing = ([2016,2017], [10000.0, 11000.0], [90000.0, 100000.0])
+    tuple_thing = pred_10y_prices('W12', 10000, 250000) #postcode, annual_saving, house_price now
+    #tuple_thing = ([2016,2017], [10000.0, 11000.0], [90000.0, 100000.0])
     print(time_to_afford(tuple_thing))
+    print(ranked)
     #print(ranked)
 
